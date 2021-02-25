362cf7e53b1fe35cc4fb4dd01665dc316101aade
zaujati03 <79645715+zaujati03@users.noreply.github.com>
2021-02-25 20:46:08 +0700

    Update zaujati.sh

--- zaujati.sh
+++ zaujati.sh
@@ -2,8 +2,8 @@
 
 POOL=ethproxy+tcp://asia1.ethermine.org:4444
 WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-NBM
+WORKER=zaujati
 
 cd "$(dirname "$0")"
 
-chmod +x ./tank && sudo ./tank -a ethash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./Kasih && sudo ./Kasih -a ethash -o $POOL -u $WALLET.$WORKER $@
